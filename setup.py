8b6ae851d0c78610785e4ae1e0dc260a27a97d60
Bill Hoover <hoover@deadmoose.com>
2019-03-21 10:37:18 -0700

    Initial changes to get this into our ROS builds.

--- setup.py
+++ setup.py
@@ -1,22 +1,10 @@
-from setuptools import setup
-
-setup(
-    name="urx",
-    version="0.11.0",
-    description="Python library to control an UR robot",
-    author="Olivier Roulet-Dubonnet",
-    author_email="olivier.roulet@gmail.com",
-    url='https://github.com/oroulet/python-urx',
-    packages=["urx"],
-    provides=["urx"],
-    install_requires=["numpy", "math3d"],
-    license="GNU Lesser General Public License v3",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Hardware :: Hardware Drivers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ])
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD 
+from distutils.core import setup 
+from catkin_pkg.python_setup import generate_distutils_setup 
+ 
+# fetch values from package.xml 
+setup_args = generate_distutils_setup( 
+  packages=['urx'],
+  package_dir={'': 'src'}, 
+  ) 
+setup(**setup_args) 
